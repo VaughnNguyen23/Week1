8224251829d32a31280c922cbafb3e94f5d1d08d
Vaughn Nguyen <vaughnnguyen522@outlook.com>
2023-06-27 14:53:48 -0700

    Moved js inline to js script

--- script.js
+++ script.js
@@ -1,2 +1,17 @@
 // script.js
-console.log("Hello from script.js!");
+var firstName = "Vaughn";
+var lastName = "Nguyen";
+var monthsOfStudy = 4;
+var goal = "Get fluent in languages like Python and SQL";
+
+var profileElement = document.getElementById("profile");
+profileElement.textContent =
+  "My name is " +
+  firstName +
+  " " +
+  lastName +
+  ". I have been at SCC for " +
+  yearsOfStudy +
+  " years and plan to " +
+  goal +
+  " after completing my courses.";
