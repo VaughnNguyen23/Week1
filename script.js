8400a16311304449e9f35782745ea576ca3af37e
Vaughn Nguyen <vaughnnguyen522@outlook.com>
2023-06-27 22:07:22 -0700

    Fix allows variable to be grabbed from script js to make full paragraph in index

--- script.js
+++ script.js
@@ -1,17 +1,7 @@
 // script.js
-var firstName = "Vaughn";
-var lastName = "Nguyen";
-var monthsOfStudy = 4;
-var goal = "Get fluent in languages like Python and SQL";
+let firstName = "Vaughn ";
+let lastName = "Nguyen";
+let yearsOfStudy = "half a year";
+let goal = "have a knowledgeable insight on Java Script and other languages";
 
-var profileElement = document.getElementById("profile");
-profileElement.textContent =
-  "My name is " +
-  firstName +
-  " " +
-  lastName +
-  ". I have been at SCC for " +
-  yearsOfStudy +
-  " years and plan to " +
-  goal +
-  " after completing my courses.";
+
